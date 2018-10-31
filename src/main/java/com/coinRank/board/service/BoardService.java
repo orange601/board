eed0b563c783dc4406193e99e95a4907a9724288
orange601 <oltop@naver.com>
2018-10-31 18:24:56 +0900

    second commit

--- src/main/java/com/coinRank/board/service/BoardService.java
+++ src/main/java/com/coinRank/board/service/BoardService.java
@@ -1,12 +1,10 @@
-package com.coinRank.board.dao;
+package com.coinRank.board.service;
 
 import java.util.List;
 import java.util.Map;
 
-public interface BoardDao {
+public interface BoardService {
     int regContent(Map<String, Object> paramMap);
-    
-    int modifyContent(Map<String, Object> paramMap);
     
     int getContentCnt(Map<String, Object> paramMap);
     
@@ -23,9 +21,8 @@ public interface BoardDao {
     int getBoardCheck(Map<String, Object> paramMap);
     
     int delBoard(Map<String, Object> paramMap);
-    
+        
     boolean checkReply(Map<String, Object> paramMap);
     
     boolean updateReply(Map<String, Object> paramMap);
-
 }
